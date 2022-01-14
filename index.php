5545bfdbb621c65964f7b74fe867eeefad0a47dd
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-01-13 21:51:49 -0500

    Creating initial Index PHP file
    Set up sources for jQuery, Chart.JS, and Bootstrap 4.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Vote!</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<script></script>
