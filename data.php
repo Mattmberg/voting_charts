0a0e1139a4892ed0c4a2073735268aad099d49e0
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-01-21 22:17:41 -0500

    Setting up initial connection to MySQL server

--- data.php
+++ data.php
@@ -1,1 +1,12 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
 
+$connection = new mysqli($servername, $username, $password);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
