f0a4f0b09108fef3c30818d863542fa22da98c8a
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-01-31 21:47:19 -0500

    Setting up initial if statements for POST actions.

--- data.php
+++ data.php
@@ -6,7 +6,44 @@ $password = "";
 $connection = new mysqli($servername, $username, $password);
 
 if ($connection->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $connection->connect_error);
 }
 echo "Connected successfully";
+
+if(isset($_POST["action"]))
+{
+	if($_POST["action"] == 'insert')
+	{
+		$data = array(
+		);
+
+		$query = "
+		";
+
+		$statement = $connection->prepare($query);
+
+		$statement->execute($data);
+
+		echo 'done';
+	}
+
+	if($_POST["action"] == 'fetch')
+	{
+		$query = "
+		";
+
+		$result = $connection->query($query);
+
+		$data = array();
+
+		foreach($result as $row)
+		{
+			$data[] = array(
+			);
+		}
+
+		echo json_encode($data);
+	}
+}
+
 ?>
