b8b27471b5b47f57820e91d58d88e99b72573c09
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-02-02 20:59:03 -0500

    Updating to add the finished product.
    I replaced all database info with placeholders.

--- data.php
+++ data.php
@@ -2,22 +2,21 @@
 $servername = "";
 $username = "";
 $password = "";
+$dbname = "";
 
-$connection = new mysqli($servername, $username, $password);
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-echo "Connected successfully";
+$connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
 if(isset($_POST["action"]))
 {
 	if($_POST["action"] == 'insert')
 	{
 		$data = array(
+			':COLOR_COLUMN'		=>	$_POST["COLOR_COLUMN"]
 		);
 
 		$query = "
+		INSERT INTO VOTING_DB
+		(COLOR_COLUMN) VALUES (:COLOR_COLUMN)
 		";
 
 		$statement = $connection->prepare($query);
@@ -30,6 +29,9 @@ if(isset($_POST["action"]))
 	if($_POST["action"] == 'fetch')
 	{
 		$query = "
+		SELECT COLOR_COLUMN, COUNT(ID_COLUMN) AS Total 
+		FROM VOTING_DB
+		GROUP BY COLOR_COLUMN
 		";
 
 		$result = $connection->query($query);
@@ -39,6 +41,8 @@ if(isset($_POST["action"]))
 		foreach($result as $row)
 		{
 			$data[] = array(
+				'COLOR_COLUMN'		=>	$row["COLOR_COLUMN"],
+				'total'			=>	$row["Total"],
 			);
 		}
 
